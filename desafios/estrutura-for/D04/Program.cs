a2aca1abacb6a218053818fb38f5d746af2ebb90
Guilherme Miranda <guilhermemirandavg2004@gmail.com>
2025-04-11 21:31:38 -0400

    Adicionando resolução do DESAFIO 04 - ESTRUTURA FOR
    Neste commit, foi adicionado arquivos do desafio 04 após sua resolução.

---
+++ desafios/estrutura-for/D04/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System;
+using System.Globalization;
+
+namespace D04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) { 
+            
+                string[] line = Console.ReadLine().Split(' ');
+
+                int a = int.Parse(line[0]);
+                int b = int.Parse(line[1]);
+
+                if (b == 0)
+                {
+                    Console.WriteLine("Divisão impossivel");
+                }
+                else 
+                {
+                    double divisao = ((double)a / b);
+                    Console.WriteLine(divisao.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
