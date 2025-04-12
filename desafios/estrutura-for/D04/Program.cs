c2958cc188a27c168a81d690a5ee281b0c367783
Guilherme Miranda <guilhermemirandavg2004@gmail.com>
2025-04-11 21:32:52 -0400

    Alteração no código do DESAFIO 04 - ADICIONANDO PRETEXTO

--- desafios/estrutura-for/D04/Program.cs
+++ desafios/estrutura-for/D04/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿/* Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel". */
 
 using System;
 using System.Globalization;
